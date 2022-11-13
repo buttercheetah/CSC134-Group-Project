f8fb7781f891a0f1bd401a03afb88baa75770422
buttercheetah <buttercheetah@gmail.com>
2022-11-13 17:12:26 -0500

    Very simplistic template

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+/*
+    Jedidiah Lee, Noah Liby, and Ian MacAlalag
+	Midterm Project
+	CSC-134
+*/
+
+// Define student class here
+
+
+// Define ClassRoom class here
+
+int main()
+{
+	// declare all variables here
+    
+
+	// read from studentInfo
+	ifstream inFile("studentInfo.txt");
+
+	while (inFile)
+	{
+		
+		
+
+
+	}
+	// close the data file, write your code here
+	inFile.close();
+
+    //sample for loop
+	for (int i = 0; i < Class to iterrate ; i++)
+	{
+
+
+	}
+	return 0;
+}
